776fd272f2c7821d1c4f26bc1416068f9c4de3b9
David Siegel <dasiege@microsoft.com>
2017-08-25 14:03:49 -0700

    Add failing test for empty object when verbose=false
    This test reveals an infinite loop.

---
+++ test/cases/right/object-only-no-verbose.js
@@ -0,0 +1,10 @@
+var types = require('../../types');
+var location = types.location;
+var object = types.createObject;
+
+module.exports = {
+	ast: object([]),
+	options: {
+		verbose: false
+	}
+};
