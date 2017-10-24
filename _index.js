ca546a97e7672927ae062de825b759a9a56ebbe1
Vladislav Trushin <vtrushin@avito.ru>
2017-10-24 15:33:13 +0300

    Changed tests

--- _index.js
+++ _index.js
@@ -1,16 +1,6 @@
 var parse = require('./build');
 var fs = require('fs');
 
-function readFile(file) {
-	var src = fs.readFileSync(file, 'utf8');
-	// normalize line endings
-	src = src.replace(/\r\n/, '\n');
-	// remove trailing newline
-	src = src.replace(/\n$/, '');
-
-	return src;
-}
-
-var a = parse(readFile('./_index.json'));
+var a = parse(fs.readFileSync('"\/"', 'utf8'));
 
 console.log(a.children[0].key);
--- test/fixtures/valid/exponential-numbers.js
+++ test/fixtures/valid/exponential-numbers.js
@@ -4,9 +4,9 @@ var literal = types.createLiteral;
 
 var ast = array([
 	literal(1, '1'),
-	number(1.2, '1.2'),
-	literal(1200, '1.2e3'),
-	literal(0.0012, '1.2e-3')
+	literal(1.2, '1.2'),
+	literal(1.2e3, '1.2e3'),
+	literal(1.2e-3, '1.2e-3')
 ]);
 
 module.exports = {
